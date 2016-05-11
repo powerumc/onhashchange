24a2c8bce9701736ab3da1171215012589da5310
Junil Um <powerumc@gmail.com>
2016-05-11 23:14:43 +0900

    change to inherit event object

--- src/onhashchange.js
+++ src/onhashchange.js
@@ -1,4 +1,25 @@
 (function() {
+    var msie = document.documentMode;
+    
+    function copyOwnProperty(src, dest) {
+        for (var p in src) {
+            if (src.hasOwnProperty(p)) { dest[p] = p; }
+        }
+    }
+    function createEvent(name, arg) {
+        var event = null;
+        if (msie && msie == 8 && document.createEventObject) {
+            event = document.createEventObject(name);
+        }
+        else if (msie && msie > 8 && document.createEvent) {
+            event = document.createEvent("CustomEvent", { obj: arg });
+            event.initEvent(name, true, true);
+        } else if (window.CustomEvent) {
+            event = new CustomEvent(name, { detail: arg });
+        }
+
+        return event;
+    }
     
     var HashChangeEvent = function(type) {
         this.isTrusted = true;
@@ -6,6 +27,11 @@
         this.newURL = "";
         this.type = type;
     }
+    
+    HashChangeEvent.prototype = createEvent("hashchange");
+    HashChangeEvent.prototype.constructor = HashChangeEvent;
+    
+    copyOwnProperty(Event, HashChangeEvent);
 
     function init() {
         var hash = location.hash;
