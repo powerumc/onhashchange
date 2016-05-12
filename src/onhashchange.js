073af6e97b17e4a90b3b5a665cd440643428b0e1
Junil Um <powerumc@gmail.com>
2016-05-13 08:40:26 +0900

    detect browser supports.

--- src/onhashchange.js
+++ src/onhashchange.js
@@ -33,6 +33,28 @@
     
     copyOwnProperty(Event, HashChangeEvent);
 
+    function checkBrowserSupport() {
+        var b = (function(){
+            var ua= navigator.userAgent, tem,
+            M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+            if(/trident/i.test(M[1])){
+                tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+                return 'IE '+(tem[1] || '');
+            }
+            if(M[1]=== 'Chrome'){
+                tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+                if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+            }
+            M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+            if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+            return M.join(' ');
+        })();
+        
+        return ((b.indexOf("Safari") > 0    && b >= "Safari 5") ||
+                (b.indexOf("IE") > 0        && b >= "IE 8") ||
+                (b.indexOf("Chrome") > 0    && b >= "Chrome 50"));
+    }
+
     function init() {
         var hash = location.hash;
         var url = location.href;
